db0d34235c46c5419c4251947e68e6d4ea742a2c
JuceDoesThings <seth.weiand@example.com>
2023-05-14 14:11:25 -0500

    Initial Commit

---
+++ src/main/java/net/jucedoesthings/timber_tech/TimberTech.java
@@ -0,0 +1,14 @@
+package net.jucedoesthings.timber_tech;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TimberTech implements ModInitializer {
+	public static final String MOD_ID = "timber_tech";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/jucedoesthings/timber_tech/TimberTechClient.java
@@ -0,0 +1,10 @@
+package net.jucedoesthings.timber_tech;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TimberTechClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
